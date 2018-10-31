08534d6d41e5a5c2afa51b618e57b0d18199977b
Josee <info@joseewouters.nl>
2018-10-31 11:24:00 +0100

    Edit typo in privacy policy

--- pages/whatdinner/index.vue
+++ pages/whatdinner/index.vue
@@ -1,8 +1,8 @@
 <template lang="md">
-    # Privacy Policy What Dinner?
+# Privacy Policy What Dinner?
 
 ## Data storage
-The meals and ingredients you put in What Dinner?are saved in the local storage of your device. This means that no one else has access to your data. It also means your meals cannot be restored when you switch phones or when you accidentally delete your meals.
+The meals and ingredients you put in What Dinner? are saved in the local storage of your device. This means that no one else has access to your data. It also means your meals cannot be restored when you switch phones or when you accidentally delete your meals.
 
 ## Access
 What Dinner? has no access to your contacts, your photos or anything else on your device.
