7267f5171c7d65b35b8bdedf54ec2c324429e688
Josee <info@joseewouters.nl>
2018-10-31 11:18:50 +0100

    Add privacy policy for What Dinner app

---
+++ pages/whatdinner/index.vue
@@ -0,0 +1,25 @@
+<template lang="md">
+    # Privacy Policy What Dinner?
+
+## Data storage
+The meals and ingredients you put in What Dinner?are saved in the local storage of your device. This means that no one else has access to your data. It also means your meals cannot be restored when you switch phones or when you accidentally delete your meals.
+
+## Access
+What Dinner? has no access to your contacts, your photos or anything else on your device.
+
+## Personal Data
+What Dinner? doesn't ask personal data of you. You do not have to create an account to use the app, you remain anonymous when you use the app.
+
+</template>
+
+<script>
+export default {
+    layout: 'blog',
+    head: {
+        title: 'Privacy policy What Dinner?',
+        meta: [
+            { hid: 'description', name: 'description', content: 'What Dinner has no access to you, your device, your personal data or to the meals you store.' }
+        ]
+    }
+}
+</script>
