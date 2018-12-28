c0eaebc0be1648a0c73446ca730d9be2c68adabb
Josee <info@joseewouters.nl>
2018-12-28 12:59:48 +0100

    Add content/blog folder structure for blogs with Netlify CMS

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,20 @@
+<template lang="md">
+    <h1>{{ title }}</h1>
+    {{ body }}
+</template>
+
+<script>
+export default {
+    layout: 'blog',
+    data (){
+        return {
+            title: '',
+            body: '',
+        }
+    },
+    async asyncData({ params }) {
+        let post = await import('~/content/blog/' + params.slug + '.md');
+        return post;
+    }
+}
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,15 @@
+<template lang="md">
+    # Blog
+</template>
+
+<script>
+export default {
+    layout: 'blog',
+    head: {
+        title: 'Blogs',
+        meta: [
+            { hid: 'description', name: 'description', content: '' }
+        ]
+    }
+}
+</script>
