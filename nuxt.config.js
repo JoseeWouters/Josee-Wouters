05d2500c844fd137c72dd30bcde8e9d308b4f529
Josee <info@joseewouters.nl>
2019-07-11 14:46:19 +0200

    Change SEO meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -148,21 +148,16 @@ module.exports = {
         ]
     },
     head: {
-        title: 'Josee Wouters - Front-end developer',
         titleTemplate: '%s - JoseeWouters.nl',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: 'Josee Wouters is een front-end developer in Nijmegen, momenteel beschikbaar voor freelance front-end projecten. Uurtarief: € 65,-' },
-            { hid: 'og:title', name: 'og:title', property: 'og:title', content: 'Josee Wouters - Front-end developer' },
+            { hid: 'description', name: 'description', content: '' },
             { hid: 'og:type', name: 'og:type', property:'og:type', content: 'website' },
-            { hid: 'og:description', name: 'og:description', property:'og:description', content: 'Josee Wouters is een front-end developer in Nijmegen, momenteel beschikbaar voor freelance front-end projecten. Uurtarief: € 65,-' },
             { hid: 'og:image', name: 'og:image', property:'og:image', content: 'https://joseewouters.nl/_nuxt/img/toetsenbord.27ab1e1.jpg' },
             { hid: 'og:url', name: 'og:url', property:'og:url', content: 'https://joseewouters.nl' },
             { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
             { hid: 'twitter:site', name: 'twitter:site', content: '@codergirlnl' },
-            { hid: 'twitter:title', name: 'twitter:title', content: 'Josee Wouters - Front-end developer' },
-            { hid: 'twitter:description', name: 'twitter:description', content: 'Josee Wouters is een front-end developer in Nijmegen, momenteel beschikbaar voor freelance front-end projecten. Uurtarief: € 65,-' },
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
--- pages/index.vue
+++ pages/index.vue
@@ -1,122 +1,110 @@
 <template>
-    <main class="resume">
-        <article v-for="item in allItems" :key="item.title" class="article">
-            <div v-if="item.layout === 'work'" class="work">
-                <p class="date">
-                    {{item.displayDate}}
-                </p>
-                <h2 class="title">
-                    {{item.title}}
-                </h2>
-                <p class="client">
-                    {{item.client}}
-                </p>
-                <div class="intro">
-                    {{item.intro}}
-                </div>
-                <a v-if="item.url" :href="item.url">Bekijk {{item.title}}</a>
-            </div>
-            <div v-if="item.layout === 'game'" class="game">
-                <h2 class="title">
-                    {{item.title}} - {{item.console}}
-                </h2>
-            </div>
-            <div v-if="item.layout === 'blog'" class="blog">
-                <p class="date">
-                    {{item.displayDate}}
-                </p>
-                <h2 class="title">
-                    {{item.title}}
-                </h2>
-                <div class="intro">
-                    {{item.intro}}
-                </div>
-                <a v-if="item.url" :href="item.url">Lees {{item.title}}</a>
-                <nuxt-link v-else :to="/blog/ + item.slug">Lees {{item.title}}</nuxt-link>
-            </div>
-            <div v-if="item.layout === 'book'" class="book">
-                <h2 class="title">
-                    {{item.title}} - {{item.author}}
-                </h2>
-            </div>
-            <div v-if="item.layout === 'study'" class="study">
-                <p class="date">
-                    {{item.displayDate}}
-                </p>
-                <h2 class="title">
-                    {{item.title}}
-                </h2>
-            </div>
-        </article>
-    </main>
+  <main class="resume">
+    <article v-for="item in allItems" :key="item.title" class="article">
+      <div v-if="item.layout === 'work'" class="work">
+        <p class="date">{{item.displayDate}}</p>
+        <h2 class="title">{{item.title}}</h2>
+        <p class="client">{{item.client}}</p>
+        <div class="intro">{{item.intro}}</div>
+        <a v-if="item.url" :href="item.url">Bekijk {{item.title}}</a>
+      </div>
+      <div v-if="item.layout === 'game'" class="game">
+        <h2 class="title">{{item.title}} - {{item.console}}</h2>
+      </div>
+      <div v-if="item.layout === 'blog'" class="blog">
+        <p class="date">{{item.displayDate}}</p>
+        <h2 class="title">{{item.title}}</h2>
+        <div class="intro">{{item.intro}}</div>
+        <a v-if="item.url" :href="item.url">Lees {{item.title}}</a>
+        <nuxt-link v-else :to="/blog/ + item.slug">Lees {{item.title}}</nuxt-link>
+      </div>
+      <div v-if="item.layout === 'book'" class="book">
+        <h2 class="title">{{item.title}} - {{item.author}}</h2>
+      </div>
+      <div v-if="item.layout === 'study'" class="study">
+        <p class="date">{{item.displayDate}}</p>
+        <h2 class="title">{{item.title}}</h2>
+      </div>
+    </article>
+  </main>
 </template>
 
 <script>
-import _ from 'lodash';
+import _ from "lodash";
 
 export default {
-    layout: 'home',
-    data () {
-
-        return {
-            all: process.env.contents.all
-        }
-    },
-    computed: {
-        allItems: function () {
-            return _.orderBy(this.all, 'date', 'desc')
-        },
+  data() {
+    return {
+      all: process.env.contents.all
+    };
+  },
+  computed: {
+    allItems: function() {
+      return _.orderBy(this.all, "date", "desc");
     }
-}
-
+  },
+  head() {
+    return {
+      title: "Josee Wouters - Front-end developer",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Josee Wouters is een front-end developer in Nijmegen, momenteel beschikbaar voor freelance front-end projecten. Uurtarief: € 65,-"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style lang="scss">
 .resume {
-    max-width: 600px;
-    margin-left: auto;
-    margin-right: auto;
+  max-width: 600px;
+  margin-left: auto;
+  margin-right: auto;
 }
 .article {
-    position: relative;
-    margin-bottom: 1rem;
-    > div {
-        margin-left: 50px;
-        &::before {
-            content: '';
-            background-image: url('~static/media/icons/pen.svg');
-            background-size: contain;
-            display: block;
-            width: 25px;
-            height: 25px;
-            position: absolute;
-            left: 0;
-            top: 0;
-        }
-        &.game::before {
-            background-image: url('~static/media/icons/game.svg');
-        }
-        &.study::before {
-            background-image: url('~static/media/icons/study.svg');
-        }
-        &.work::before {
-            background-image: url('~static/media/icons/work.svg');
-        }
-        &.book::before {
-            background-image: url('~static/media/icons/book.svg');
-        }
+  position: relative;
+  margin-bottom: 1rem;
+  > div {
+    margin-left: 50px;
+    &::before {
+      content: "";
+      background-image: url("~static/media/icons/pen.svg");
+      background-size: contain;
+      display: block;
+      width: 25px;
+      height: 25px;
+      position: absolute;
+      left: 0;
+      top: 0;
     }
-    .date {
-        font-size: .9rem;
-        font-style: italic;
+    &.game::before {
+      background-image: url("~static/media/icons/game.svg");
     }
-    .client {
-        font-weight: bold;
-        margin-top: .2rem;
+    &.study::before {
+      background-image: url("~static/media/icons/study.svg");
     }
-    .intro {
-        margin-top: .2rem;
-        margin-bottom: .2rem;
+    &.work::before {
+      background-image: url("~static/media/icons/work.svg");
     }
-} 
+    &.book::before {
+      background-image: url("~static/media/icons/book.svg");
+    }
+  }
+  .date {
+    font-size: 0.9rem;
+    font-style: italic;
+  }
+  .client {
+    font-weight: bold;
+    margin-top: 0.2rem;
+  }
+  .intro {
+    margin-top: 0.2rem;
+    margin-bottom: 0.2rem;
+  }
+}
 </style>
