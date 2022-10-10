7355c628f5ae2feae6db156c168a0f9dd736b37a
Josee <info@joseewouters.nl>
2022-10-10 14:37:00 +0200

    add page for toddler turf

---
+++ pages/toddler-turf.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="content">
+    <h1>Toddler Turf</h1>
+    <p class="intro">
+      Toddler Turf: educatieve spelletjes voor peuters. Gratis, in het Nederlands en geen advertenties! Meer spelletjes en variaties volgen.
+    </p>
+    <h2>Privacy Policy</h2>
+    <p>Toddler Turf is volledig gratis, heeft geen advertenties en verzamelt geen data. Jij en je peuter kunnen deze app dus met een gerust hart gebruiken.</p>
+  </main>
+</template>
